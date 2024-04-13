10b6b6756a643390fa6681abbaed6d8af65ce71f
vinnidias <vinicius.3991@gmail.com>
2024-04-13 18:29:16 -0300

    ajuste de dominio de imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["https://mks-sistemas.nyc3.digitaloceanspaces.com/"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mks-sistemas.nyc3.digitaloceanspaces.com',
+        port: '',
+        pathname: '/products/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
