af8e9623e24d3e34299357b935ea8b84ae050cd2
vinnidias <vinicius.3991@gmail.com>
2024-04-14 17:06:02 -0300

    aumentando stale time

--- src/service/queryClient.ts
+++ src/service/queryClient.ts
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 5,
+      staleTime: 1000 * 60,
     },
   },
 });
