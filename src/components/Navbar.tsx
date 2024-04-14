48a814517e030c946e88b175ea70d2529d7423a1
vinnidias <vinicius.3991@gmail.com>
2024-04-14 17:03:59 -0300

    ajustes logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,11 @@ import ShoppingCart from "./ShoppingCart";
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center min-w-screen min-h-[100px] px-24 bg-[#0F52BA]">
-      <p className="text-white">MKS sistemas</p>
+      <div className="flex items-center gap-2">
+        <p className="text-white text-4xl font-semibold">MKS </p>
+        <p className="text-white text-xl">sistemas</p>
+      </div>
+
       <ShoppingCart />
     </nav>
   );
