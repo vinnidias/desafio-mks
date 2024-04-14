f44162bf6643c79e0943aa578bd85c82a1d68873
vinnidias <vinicius.3991@gmail.com>
2024-04-14 13:27:45 -0300

    componente footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+  return(
+    <div className="flex min-h-8 min-full-screen items-center justify-center bg-[#EEEEEE] bottom-0">
+      <p className="text-xs">MKS sistemas © Todos os direitos reservados</p>
+    </div>
+  )
+}
