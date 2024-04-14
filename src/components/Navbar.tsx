08dd24454050d0101fd05478dc921d95bcbd1261
vinnidias <vinicius.3991@gmail.com>
2024-04-14 13:28:35 -0300

    componentizando modal do carrinho

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
-import cartIcont from "../assets/Vector.svg"
+"use client";
+
+import { useCartContext } from "@/context/cartContext";
+import ShoppingCart from "./ShoppingCart";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center min-w-screen min-h-[100px] px-24 bg-[#0F52BA]">
+    <nav className="flex justify-between items-center min-w-screen min-h-[100px] px-24 bg-[#0F52BA]">
       <p className="text-white">MKS sistemas</p>
-      <button className="flex min-w-24 min-h-12 justify-center items-center gap-4 rounded-md bg-white font-bold"><Image alt="ícone do carrinho" src={cartIcont}/> 0 </button>
-    </div>
+      <ShoppingCart />
+    </nav>
   );
 }
